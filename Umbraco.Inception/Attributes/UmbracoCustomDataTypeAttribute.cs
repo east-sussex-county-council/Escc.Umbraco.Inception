23012a41fb82ae54d21850fc537c74bdfe207bd1
ehabt <ehab.taha@eastsussex.gov.uk>
2014-10-07 15:22:59 +0100

    Added new feature to add custom data types

---
+++ Umbraco.Inception/Attributes/UmbracoCustomDataTypeAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core.Models;
+using Umbraco.Core.Services;
+
+namespace Umbraco.Inception.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class UmbracoCustomDataTypeAttribute : Attribute
+    {
+
+        public string DataTypeName { get; set; }
+        public string PropertyEditorAlias { get; set; }
+        public Type PreValues { get; set; }
+
+        /// <summary>
+        /// Put this on the class that represents your data type you want to create and insert into the database.
+        /// </summary>
+        /// <param name="dataTypeName">Friendly name of the data type</param>
+        /// <param name="propertyEditorAlias">Alias of the data type</param>
+        /// <param name="preValues">Initial values</param>      
+        public UmbracoCustomDataTypeAttribute(string dataTypeName, string propertyEditorAlias, Type preValues)
+        //public UmbracoCustomDataTypeAttribute(string dataTypeName, string propertyEditorAlias, Type preValues)
+        {
+            DataTypeName = dataTypeName;
+            PropertyEditorAlias = propertyEditorAlias;
+            //PreValues = preValues;
+            PreValues = preValues;
+        }
+    }
+}
